0ecccab7edc8f17f5b5199173b7079663c4df51d
Adhithya Ramakumar <adi@Adhithyas-iMac.local>
2019-03-02 18:57:19 -0800

    is this committing

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to components page</Link>
+    <Link to="/page-2/">Go to all components page</Link>
   </Layout>
 )
 
